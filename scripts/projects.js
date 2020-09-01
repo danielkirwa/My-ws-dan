61a210ed8f2db3cb4823d7f673a90351fe39fc53
danielkirwa <kirwadan03@gmail.com>
2020-09-01 15:37:00 +0300

    modified:   Home.html 	modified:   Links.html 	modified:   Projects.html 	new file:   scripts/projects.js 	modified:   styles/links.css 	new file:   styles/projects.css

---
+++ scripts/projects.js
@@ -0,0 +1,18 @@
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// hid nav bar
+
+			var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-400px";
+  }
+  prevScrollpos = currentScrollPos;
+}
