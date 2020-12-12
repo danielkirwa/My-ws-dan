79585a942c10e8f63968071bfc0b522aa6804302
danielkirwa <kirwadan03@gmail.com>
2020-12-12 22:51:53 +0300

    modified file

--- scripts/index2.js
+++ scripts/index2.js
@@ -37,6 +37,85 @@ var currentScrollPos = window.pageYOffset;
   prevScrollpos = currentScrollPos;
 }
 
+// add action to menu icon
+
+let btnmenushow = document.querySelector('.btnmenu');
+btnmenushow.addEventListener( 'click', () =>{
+  let checkmenucode = localStorage.getItem('showmenu');
+  checkmenucode = parseInt(checkmenucode);
+ if(checkmenucode == 1){
+  document.querySelector('.submenu-menu').style.opacity = "0";
+  localStorage.setItem('showmenu', 0);
+}else if(checkmenucode == 0){
+   document.querySelector('.submenu-menu').style.opacity = "1";
+   localStorage.setItem('showmenu', 1);
+}else{
+  localStorage.setItem('showmenu', 1);
+  document.querySelector('.submenu-menu').style.opacity = "1";
+}
+
+
+}) 
+
+// call resume or portfolio
+let callportfolio = document.querySelector('.slidemenup');
+if(callportfolio == null){
+
+}else{
+  callportfolio.addEventListener('click', () =>{
+  document.querySelector('.Portfolio').style.display = "block";
+  document.querySelector('.resumecontent').style.display = "none";
+  localStorage.setItem('displaycontent', 1);
+})
+}
+let callresume = document.querySelector('.slidemenur');
+if (callresume == null) {
+
+}else{
+callresume.addEventListener('click', () =>{
+  document.querySelector('.Portfolio').style.display = "none";
+  document.querySelector('.resumecontent').style.display = "block";
+  localStorage.setItem('displaycontent', 2);
+})
+}
+
+
+
+
+
+function keepsubmenu() {
+  // body...
+   let checkmenucode = localStorage.getItem('showmenu'); 
+   if (checkmenucode == 1) {
+     document.querySelector('.submenu-menu').style.opacity = "1";
+   }else if(checkmenucode == 0){
+    document.querySelector('.submenu-menu').style.opacity = "0";
+  }else{
+
+  }
+
+}
+
+function keepcontent() {
+  // body...
+   let checkcontent = localStorage.getItem('displaycontent'); 
+   if (checkcontent == 1) {
+    document.querySelector('.Portfolio').style.display = "block";
+  document.querySelector('.resumecontent').style.display = "none";
+   }else if(checkcontent == 2){
+     document.querySelector('.Portfolio').style.display = "none";
+  document.querySelector('.resumecontent').style.display = "block";
+  }else{
+    localStorage.setItem('displaycontent', 1);
+    document.querySelector('.Portfolio').style.display = "block";
+  document.querySelector('.resumecontent').style.display = "none";
+  }
+
+}
+
+
+ keepcontent();
+keepsubmenu();
 
 
 
