1ef90e861cf69dcabd9296b44fa6068ffe65a844
danielkirwa <kirwadan03@gmail.com>
2020-12-14 17:01:19 +0300

    modified file added bottom scroll animation

--- scripts/index2.js
+++ scripts/index2.js
@@ -123,6 +123,29 @@ function keepcontent() {
 
 }
 
+// animate typing effect
+const texts = ['Web Developer','Android Developer'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+(function typing() {
+  // body...
+  if(count === texts.length){
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+  document.querySelector('.myprofession').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(typing , 600);
+
+}());
+
+
 
  keepcontent();
 keepsubmenu();
