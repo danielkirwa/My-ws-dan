dce691af6f1ea555b0d41d21577b9b31bb37740c
danielkirwa <kirwadan03@gmail.com>
2021-06-17 18:00:08 +0300

    New snap design added on upgrade

--- scripts/index2.js
+++ scripts/index2.js
@@ -2,15 +2,6 @@
 var mybutton = document.getElementById("up");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
@@ -24,129 +15,13 @@ function topFunction() {
   }
 };
 
-// hid nav bar
 
-			var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-400px";
-  }
-  prevScrollpos = currentScrollPos;
-}
 
 // add action to menu icon
 
-let btnmenushow = document.querySelector('.btnmenu');
-btnmenushow.addEventListener( 'click', () =>{
-  let checkmenucode = localStorage.getItem('showmenu');
-  checkmenucode = parseInt(checkmenucode);
- if(checkmenucode == 1){
-  document.querySelector('.submenu-menu').style.opacity = "0";
-  document.querySelector('.submenu-menu').style.pointerEvents = "none";
-  localStorage.setItem('showmenu', 0);
-}else if(checkmenucode == 0){
-   document.querySelector('.submenu-menu').style.opacity = "1";
-   document.querySelector('.submenu-menu').style.pointerEvents = "auto";
-   localStorage.setItem('showmenu', 1);
-}else{
-  localStorage.setItem('showmenu', 1);
-  document.querySelector('.submenu-menu').style.opacity = "1";
-  document.querySelector('.submenu-menu').style.pointerEvents = "auto";
-}
 
-
-}) 
 
 // call resume or portfolio
-let callportfolio = document.querySelector('.slidemenup');
-if(callportfolio == null){
-
-}else{
-  callportfolio.addEventListener('click', () =>{
-  document.querySelector('.Portfolio').style.display = "block";
-  document.querySelector('.selectedtab').innerHTML = "Portfolio";
-  document.querySelector('.Feedback').style.display = "none";
-  document.querySelector('.resumecontent').style.display = "none";
-  localStorage.setItem('displaycontent', 1);
-})
-}
-let callresume = document.querySelector('.slidemenur');
-if (callresume == null) {
-
-}else{
-callresume.addEventListener('click', () =>{
-  document.querySelector('.Portfolio').style.display = "none";
-  document.querySelector('.Feedback').style.display = "none";
-  document.querySelector('.resumecontent').style.display = "block";
-  document.querySelector('.selectedtab').innerHTML = "Resume";
-  localStorage.setItem('displaycontent', 2);
-})
-}
-
-let callfeedback = document.querySelector('.slidemenuf');
-if (callfeedback == null) {
-
-}else{
-callfeedback.addEventListener('click', () =>{
-  document.querySelector('.Portfolio').style.display = "none";
-  document.querySelector('.resumecontent').style.display = "none";
-  document.querySelector('.Feedback').style.display = "block";
-  document.querySelector('.selectedtab').innerHTML = "Feedback";
-  localStorage.setItem('displaycontent', 3);
-})
-}
-
-
-
-
-
-function keepsubmenu() {
-  // body...
-   let checkmenucode = localStorage.getItem('showmenu'); 
-   if (checkmenucode == 1) {
-     document.querySelector('.submenu-menu').style.opacity = "1";
-     document.querySelector('.submenu-menu').style.pointerEvents = "auto";
-   }else if(checkmenucode == 0){
-    document.querySelector('.submenu-menu').style.opacity = "0";
-    document.querySelector('.submenu-menu').style.pointerEvents = "none";
-  }else{
-
-  }
-
-}
-
-function keepcontent() {
-  // body...
-   let checkcontent = localStorage.getItem('displaycontent'); 
-   if (checkcontent == 1) {
-    document.querySelector('.Feedback').style.display = "none";
-  document.querySelector('.resumecontent').style.display = "none";
-  document.querySelector('.Portfolio').style.display = "block";
-  document.querySelector('.selectedtab').innerHTML = "Portfolio";
-   }else if(checkcontent == 2){
-     document.querySelector('.Portfolio').style.display = "none";
-     document.querySelector('.Feedback').style.display = "none";
-  document.querySelector('.resumecontent').style.display = "block";
-  document.querySelector('.selectedtab').innerHTML = "Resume";
-  }else if(checkcontent == 3){
-    document.querySelector('.Portfolio').style.display = "none";
-  document.querySelector('.resumecontent').style.display = "none";
-  document.querySelector('.Feedback').style.display = "block";
-    document.querySelector('.selectedtab').innerHTML = "Feedback";
-
-  }else{
-    localStorage.setItem('displaycontent', 1);
-  document.querySelector('.resumecontent').style.display = "none";
-   document.querySelector('.Feedback').style.display = "none";
-   document.querySelector('.Portfolio').style.display = "block";
-    document.querySelector('.selectedtab').innerHTML = "Portfolio";
-  }
-
-}
-
 // animate typing effect
 const texts = ['a Web Developer  ','an Android Developer  '];
 let count = 0;
@@ -177,9 +52,6 @@ function getdate() {
   displayyear.innerHTML = thisyear;
 }
 
-
- keepcontent();
-keepsubmenu();
 getdate();
 
 
