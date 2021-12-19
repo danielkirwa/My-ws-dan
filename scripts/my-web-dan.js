9f30b4d3961501075c72007c6465702e9900b5a9
danielkirwa <kirwadan03@gmail.com>
2021-12-20 00:52:37 +0300

    visitors countor added

--- scripts/my-web-dan.js
+++ scripts/my-web-dan.js
@@ -19,3 +19,92 @@ let letter = '';
   setTimeout(typing , 400);
 
 }());
+
+// visitors counter
+
+
+
+
+
+    
+//firebase configerations 
+
+var firebaseConfig = {
+   apiKey: "AIzaSyArL98ueDDOZhgnS0Z9PAOfKgP8MUcrho0",
+    authDomain: "e-learning-app-d98c7.firebaseapp.com",
+    databaseURL: "https://e-learning-app-d98c7-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "e-learning-app-d98c7",
+    storageBucket: "e-learning-app-d98c7.appspot.com",
+    messagingSenderId: "17597297186",
+    appId: "1:17597297186:web:e724ebb9903c428f96907b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//insertcount() ;
+function insertcount() {
+  // body...
+    let visitorscount = "Visitorscountnumber"
+  firebase.database().ref('Visitors/' + visitorscount).set({
+
+      CountNumber: "0"
+
+    },  (error) => {
+  if (error) {
+    // The write failed...
+     alert('Registration Faled');
+     
+  }else{
+ alert('Count added');
+     
+  }
+}
+)
+}
+
+
+let previouscount;
+getallvisitors();
+//get previouse count
+let countholder = document.getElementById('countHolder');
+function getallvisitors() {
+  // body...
+  let visitorscount = "Visitorscountnumber"
+  
+  firebase.database().ref('Visitors/' + visitorscount).on('value',function(snapshot){
+    try{
+
+      previouscount = snapshot.val().CountNumber;
+      localStorage.setItem('Count',previouscount );
+      countholder.innerHTML = previouscount;
+  }catch(err){
+    //alert(typeof err);
+    console.log(err.message);
+
+ 
+  } 
+
+})
+  
+}
+setTimeout(function() { updatevisitors(previouscount); }, 5000);
+// push the current count
+function updatevisitors(newcount) {
+  // body...
+  let visitorscount = "Visitorscountnumber"
+    firebase.database().ref('Visitors/' + visitorscount).update({
+
+    CountNumber: newcount+1
+      }).then(() => {
+  // Data saved successfully!
+  
+})
+.catch((error) => {
+  console.log('counter failed')
+});
+}
+
+// get current year
+
+let currentyear = document.getElementById('currentyear');
+let year = new Date();
+currentyear.innerHTML = year.getFullYear();
