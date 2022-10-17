fc29d4d54fb902f1573f9534dff9516e587b9df0
danielkirwa <kirwadan03@gmail.com>
2022-10-17 10:29:57 +0300

    Add and fixed more cert button

---
+++ scripts/morecert.js
@@ -0,0 +1,12 @@
+
+var x = document.getElementById("morecert1");
+x.style.display = "none";
+function MoreCert1() {
+	// body...
+	   
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
+} 
