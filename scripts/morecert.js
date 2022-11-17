8c697a3441a99071338c071de385aa0148b8e7ad
danielkirwa <kirwadan03@gmail.com>
2022-11-17 10:01:52 +0300

    Working on more certification uploading and viewing

--- scripts/morecert.js
+++ scripts/morecert.js
@@ -1,12 +1,15 @@
 
 var x = document.getElementById("morecert1");
+var btnviewone = document.getElementById("btnviewone");
 x.style.display = "none";
 function MoreCert1() {
 	// body...
 	   
   if (x.style.display === "none") {
     x.style.display = "flex";
+    btnviewone.innerHTML = "Hide Certificate";
   } else {
     x.style.display = "none";
+    btnviewone.innerHTML = "See more certificates ...";
   }
 } 
